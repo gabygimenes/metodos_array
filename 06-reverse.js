bcd087754c2ad78c38632a8538658a4c719b5416
gabygimenes <gabriela.gimenes@edu.senai.br>
2026-08-27 10:43:35 -0300

    ex6

---
+++ 06-reverse.js
@@ -0,0 +1,17 @@
+console.log("=== 6. reverse() ===");
+
+//exemplo1 - números
+const numeros = [1,2,3,4,5];
+numeros.reverse();
+console.log("Números invertidos:",numeros);
+
+//exemplo2 - ranking
+const ranking = [
+    "João",
+    "Maria",
+    "Carlos",
+    "Ana"
+];
+
+ranking.reverse();
+console.log("Ranking invertido:", ranking);
